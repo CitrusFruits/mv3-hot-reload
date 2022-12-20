33a2e765122a712bced042b5f82cc3d2a8bc9b4d
Andrew Duensing <andrew+gitlab@getmagical.com>
2022-12-20 09:54:30 -0800

    close the websocket before reloading the page

--- src/content.ts
+++ src/content.ts
@@ -4,6 +4,9 @@ if (isDev) {
   const ws = new WebSocket(`ws://localhost:${PORT}`)
   ws.addEventListener('message', (event) => {
     if (event.data === Message.FileChange) {
+      // Close the websocket since it will reconnect after reloading.
+      ws.close()
+
       chrome.runtime.sendMessage(Message.Reload)
     }
   })
